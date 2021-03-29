320a57c8f5534fb4e44f27e64fdc6c9c38206afd
streamlineddesigns <streamline.our.designs@gmail.com>
2021-03-29 02:27:47 -0400

    Adding assets, adding gitignore

---
+++ Assets/StudioByStorm/Scripts/DeformableSnow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeformableSnow : MonoBehaviour
+{
+    protected Mesh deformingMesh;
+	protected Vector3[] originalVertices;
+    protected Vector3[] displacedVertices;
+    protected Vector3[] vertexVelocities;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deformingMesh = GetComponent<MeshFilter>().mesh;
+		originalVertices = deformingMesh.vertices;
+		displacedVertices = new Vector3[originalVertices.Length];
+		for (int i = 0; i < originalVertices.Length; i++) {
+			displacedVertices[i] = originalVertices[i];
+		}
+
+        vertexVelocities = new Vector3[originalVertices.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
